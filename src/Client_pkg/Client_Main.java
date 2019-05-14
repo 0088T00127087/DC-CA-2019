175d7c1d875492806c8ae3489a4eea5f01e34ac0
joe Enright <Joseph.Enright@students.ittralee.ie>
2019-05-14 10:34:28 +0100

    Initial commit

---
+++ src/Client_pkg/Client_Main.java
@@ -0,0 +1,10 @@
+package Client_pkg;
+
+public class Client_Main {
+
+    public static void main(String [] args ){
+
+
+
+    }
+}
---
+++ src/Server_pkg/Server_Main.java
@@ -0,0 +1,11 @@
+package Server_pkg;
+
+public class Server_Main {
+    public static void main (String [] args){
+
+
+
+
+
+    }
+}
